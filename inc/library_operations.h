b66122c56afcee896d5aaa0226b23513e1626f4b
stepin104955 <69070462+stepin104955@users.noreply.github.com>
2020-10-02 20:50:02 +0530

    Update library_operations.h

--- inc/library_operations.h
+++ inc/library_operations.h
@@ -11,7 +11,7 @@
 * @param[in] operand2 
 * @return Result of operand1 + operand2
 */
-int Add A New Book(int operand1, int operand2);
+int Addbook(int operand1, int operand2);
 
 /**
 *  subtracts the operand1, operand2 and returns the result
@@ -19,7 +19,7 @@ int Add A New Book(int operand1, int operand2);
 * @param[in] operand2 
 * @return Result of operand1 - operand2
 */
-int subtract(int operand1, int operand2);
+int Searchbook(int operand1, int operand2);
 
 /**
 *  multiply the operand1, operand2 and returns the result
@@ -27,7 +27,7 @@ int subtract(int operand1, int operand2);
 * @param[in] operand2 
 * @return Result of operand1 * operand2
 */
-int multiply(int operand1, int operand2);
+int Displaybook(int operand1, int operand2);
 
 
 /**
@@ -37,6 +37,50 @@ int multiply(int operand1, int operand2);
 * @return integer value of the operand1 / operand2
 * @note returns 0 for divide by 0 error
 */
-int divide(int operand1, int operand2);
+int Author(int operand1, int operand2);
 
+/**
+* divides the operand1 by operand2 and returns the result 
+* @param[in] operand1 
+* @param[in] operand2 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+int Stock(int operand1, int operand2);
+
+/**
+* divides the operand1 by operand2 and returns the result 
+* @param[in] operand1 
+* @param[in] operand2 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+int Issue(int operand1, int operand2);
+
+/**
+* divides the operand1 by operand2 and returns the result 
+* @param[in] operand1 
+* @param[in] operand2 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+int bookret(int operand1, int operand2);
+
+/**
+* divides the operand1 by operand2 and returns the result 
+* @param[in] operand1 
+* @param[in] operand2 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+int Addmembr(int operand1, int operand2);
+
+/**
+* divides the operand1 by operand2 and returns the result 
+* @param[in] operand1 
+* @param[in] operand2 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+int Exit(int operand1, int operand2);
 #endif  /* #define __LIBRARY_OPERATIONS_H__ */
