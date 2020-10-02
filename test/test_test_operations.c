b9db8a6a0164766e04ed2fa95c927839aa5d179a
stepin104955 <69070462+stepin104955@users.noreply.github.com>
2020-10-02 21:30:42 +0530

    Update test_test_operations.c

--- test/test_test_operations.c
+++ test/test_test_operations.c
@@ -6,10 +6,16 @@
 #define PROJECT_NAME    "Library"
 
 /* Prototypes for all the test functions */
-void test_add(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
+void test_Addbook(void);
+void test_Searchbook(void);
+void test_Displaybook(void);
+void test_Author(void);
+void test_Titlelist(void);
+void test_Stock(void);
+void test_Issue(void);
+void test_bookret(void);
+void test_Addmembr(void);
+void test_Exit(void);
 
 /* Start of the application test */
 int main() {
@@ -22,10 +28,17 @@ int main() {
   
   
   /* Add your test functions in this format for testing*/
-  CU_add_test(suite, "add", test_add);
-  CU_add_test(suite, "subtract", test_subtract);
-  CU_add_test(suite, "multiply", test_multiply);
+  CU_add_test(suite, "Addbook", test_Addbook);
+  CU_add_test(suite, "Searchbookt", test_Searchbook);
+  CU_add_test(suite, "Displaybook", test_Displaybook);
+  CU_add_test(suite, "Author", test_Author);
+  CU_add_test(suite, "Titlelist", test_Titlelist);
+  CU_add_test(suite, "Stock", test_Stock);
+  CU_add_test(suite, "Issue", test_Issue);
   CU_add_test(suite, "divide", test_divide);
+  CU_add_test(suite, "bookret", test_bookret);
+  CU_add_test(suite, "Addmembr", test_Addmembr);
+  CU_add_test(suite, "Exit", test_Exit);
 
 
 /* Note: Do not edit START*/
@@ -42,30 +55,74 @@ int main() {
 }
 
 /* Write all the test functions */ 
-void test_add(void) {
-  CU_ASSERT(30 == add(10, 20));
+void test_Addbook(void) {
+  CU_ASSERT(30 == Addbook(10, 20));
   
   /* Dummy fail*/
-  CU_ASSERT(1500 == add(750, 7500));
+  CU_ASSERT(1500 == Addbook(750, 7500));
 }
 
-void test_subtract(void) {
-  CU_ASSERT(-3 == subtract(0, 3));
+void test_Searchbook(void) {
+  CU_ASSERT(-3 == Searchbook(0, 3));
   
   /* Dummy fail*/
-  CU_ASSERT(1 == subtract(1000, 900));
+  CU_ASSERT(1 == Searchbook(1000, 900));
 }
 
-void test_multiply(void) {
-  CU_ASSERT(0 == multiply(1, 0));
+void test_Displaybook(void) {
+  CU_ASSERT(0 == Displaybook(1, 0));
   
   /* Dummy fail*/
-  CU_ASSERT(2 == multiply(2, 5));
+  CU_ASSERT(2 == Displaybook(2, 5));
 }
 
-void test_divide(void) {
-  CU_ASSERT(0 == divide(1, 0));
+void test_Author(void) {
+  CU_ASSERT(0 == Author(1, 0));
   
   /* Dummy fail*/
-  CU_ASSERT(3 == divide(2, 2));
+  CU_ASSERT(3 == Author(2, 2));
+}
+
+
+void test_Titlelist(void) {
+  CU_ASSERT(0 == Titlelist(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(2 == Titlelist(2, 5));
+}
+
+
+void test_Stock(void) {
+  CU_ASSERT(0 == Stock(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(2 == Stock(2, 5));
+}
+
+void test_Issue(void) {
+  CU_ASSERT(0 == Issue(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == Issue(2, 2));
+}
+
+void test_bookret(void) {
+  CU_ASSERT(0 == bookret(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == bookret(2, 2));
+}
+
+void test_Addmembr(void) {
+  CU_ASSERT(0 == Addmembr(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(2 == Addmembr(2, 5));
+}
+
+void test_Exit(void) {
+  CU_ASSERT(0 == Exit(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == Exit(2, 2));
 }
