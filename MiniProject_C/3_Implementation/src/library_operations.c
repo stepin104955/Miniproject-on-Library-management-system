20d9b60fbe59d5ea5f5d884efb4683cb17aa0497
stepin104955 <69070462+stepin104955@users.noreply.github.com>
2020-11-14 13:11:48 +0530

    Update library_operations.c

--- MiniProject_C/3_Implementation/src/library_operations.c
+++ MiniProject_C/3_Implementation/src/library_operations.c
@@ -1,11 +1,11 @@
 #include <library_operations.h>
 
-int Addbook(int operand1, int operand2)
+int Addbook1(int operand1, int operand2)
 {
     return operand1 + operand2;
 }
 
-int Searchbook(int operand1, int operand2)
+/*int Searchbook(int operand1, int operand2)
 {
     return operand1 - operand2;
 }
@@ -43,8 +43,8 @@ int bookret(int operand1, int operand2)
 int Addmembr(int operand1, int operand2)
 {
     return operand1 * operand2;
-}
-int Exit(int operand1, int operand2)
+}*/
+int Exit0(int operand1, int operand2)
 {
     if(operand2 == 0)
         return 0;
